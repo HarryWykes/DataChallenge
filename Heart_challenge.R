9ab0f844bc52ecbca5ae40608b36a4439d8d3766
S-2009 <133561253+S-2009@users.noreply.github.com>
2023-12-07 12:30:34 +0000

    Update Heart_challenge.R

--- Heart_challenge.R
+++ Heart_challenge.R
@@ -1,3 +1,29 @@
 library(this.path)
 
-read.csv()
+#loading the data 
+
+fit <- read.csv("c:/practical_data/heart.csv")
+head(fit, 10)
+sum(is.na(fit))
+
+#Checking for missing values
+sum(is.na(fit$Cholesterol))
+table(is.na(fit$Cholesterol))
+
+#Assigning all of the zero values to NA 
+
+fit$Cholesterol[which(fit$Cholesterol <= 0)] <- NA
+table(is.na(fit$Cholesterol))
+
+fit$RestingBP[which(fit$RestingBP <= 0)] <- NA
+table(is.na(fit$RestingBP))
+
+#Cleaning the data 
+
+fit.clean_chol <- subset(fit, !is.na(fit$Cholesterol))
+str(fit.clean)
+
+fit.clean_bp <- subset(fit, !is.na(fit$RestingBP))
+str(fit.clean_bp)
+
+
