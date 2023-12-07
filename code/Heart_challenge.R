2c3b872acc4fbf8a3cce120da9c7a10a0b002f6e
HarryWykes <jx23538@bristol.ac.uk>
2023-12-07 15:40:57 +0000

    kldsjf

--- code/Heart_challenge.R
+++ code/Heart_challenge.R
@@ -3,14 +3,17 @@ library(ggplot2)
 
 #loading the data 
 
+fitr <- read.csv(paste0(this.dir(), "/../raw/heart.csv"))
+sum(fitr$Cholesterol==0)
+
 fit <- read.csv(paste0(this.dir(), "/../clean/cleaned_heart.csv"))
 
-lm<-lm(RestingBP ~ Cholesterol + Age + Sex, data=fit)
+lm<-lm(RestingBP ~ Cholesterol*Sex + Age, data=fit)
 summary(lm)
 
 fit$ageBin<-cut(fit$Age, breaks=c(-Inf, 50, Inf), labels=c("<54", ">=54"))
 
-cholBP<-ggplot(fit, aes(x=Cholesterol, y=RestingBP, col=Sex))+
+cholBPSex<-ggplot(fit, aes(x=Cholesterol, y=RestingBP, col=Sex))+
   geom_point(alpha=0.3)+
   geom_smooth(method="lm", formula=y~x, se=FALSE)+
   labs(x="Cholesterol", y="Resting Blood Pressure")
@@ -20,7 +23,8 @@ cholBPAge<-ggplot(fit, aes(x=Cholesterol, y=RestingBP, col=ageBin))+
   geom_smooth(method="lm", formula=y~x, se=FALSE)+
   labs(x="Cholesterol", y="Resting Blood Pressure", col="Age")
 
-ggsave("/../visualisations/plot1.png", plot)
+ggsave(paste0(this.dir(), "/../visualisation/cholBPSex.png"), cholBPSex)
+ggsave(paste0(this.dir(), "/../visualisation/cholBPAge.png"), cholBPAge)
 
 
 
