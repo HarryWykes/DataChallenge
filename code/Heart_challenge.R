51b4e12eebc185a4dd82d5c3ce706a425116a625
HarryWykes <jx23538@bristol.ac.uk>
2023-12-07 12:38:51 +0000

    Load clean data

--- code/Heart_challenge.R
+++ code/Heart_challenge.R
@@ -2,28 +2,35 @@ library(this.path)
 
 #loading the data 
 
-fit <- read.csv("c:/practical_data/heart.csv")
-head(fit, 10)
-sum(is.na(fit))
+fit <- read.csv(paste0(this.dir(), "/../clean/cleaned_heart.csv"))
 
-#Checking for missing values
-sum(is.na(fit$Cholesterol))
-table(is.na(fit$Cholesterol))
+# head(fit, 10)
+# sum(is.na(fit))
+# 
+# #Checking for missing values
+# sum(is.na(fit$Cholesterol))
+# table(is.na(fit$Cholesterol))
+# 
+# #Assigning all of the zero values to NA 
+# 
+# fit$Cholesterol[which(fit$Cholesterol <= 0)] <- NA
+# table(is.na(fit$Cholesterol))
+# 
+# fit$RestingBP[which(fit$RestingBP <= 0)] <- NA
+# table(is.na(fit$RestingBP))
+# 
+# #Cleaning the data 
+# 
+# fit.clean_chol <- subset(fit, !is.na(fit$Cholesterol))
+# str(fit.clean)
+# 
+# fit.clean_bp <- subset(fit, !is.na(fit$RestingBP))
+# str(fit.clean_bp)
 
-#Assigning all of the zero values to NA 
 
-fit$Cholesterol[which(fit$Cholesterol <= 0)] <- NA
-table(is.na(fit$Cholesterol))
 
-fit$RestingBP[which(fit$RestingBP <= 0)] <- NA
-table(is.na(fit$RestingBP))
 
-#Cleaning the data 
 
-fit.clean_chol <- subset(fit, !is.na(fit$Cholesterol))
-str(fit.clean)
 
-fit.clean_bp <- subset(fit, !is.na(fit$RestingBP))
-str(fit.clean_bp)
 
 
